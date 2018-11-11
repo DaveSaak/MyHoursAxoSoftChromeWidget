70c7b54da718657752c278b6d82d9df027490075
DaveSaak <44680431+DaveSaak@users.noreply.github.com>
2018-11-12 00:08:49 +0100

    added default worklog type id and notification logic

--- optionsPage.js
+++ optionsPage.js
@@ -6,25 +6,33 @@ $(function () {
     var options = new OptionsRepo.getInstance();
 
 
-    options.load(function () {
-        $('#axoSoftUrl').val(options.axoSoftUrl);
-        $('#axoSoftToken').val(options.axoSoftToken);
-        $('#axoSoftUserId').val(options.axoSoftUserId);
+    options.load().then(function () {
+            $('#axoSoftUrl').val(options.axoSoftUrl);
+            $('#axoSoftToken').val(options.axoSoftToken);
+            $('#axoSoftUserId').val(options.axoSoftUserId);
+            $('#axoSoftDefaultWorklogTypeId').val(options.axoSoftDefaultWorklogTypeId);
+    });
+    
+
+
+$('#saveButton').click(function () {
+    var options = new OptionsRepo.getInstance();
+
+    options.axoSoftUrl = $('#axoSoftUrl').val();
+    options.axoSoftToken = $('#axoSoftToken').val();
+    options.axoSoftUserId = $('#axoSoftUserId').val();
+    options.axoSoftDefaultWorklogTypeId = $('#axoSoftDefaultWorklogTypeId').val();
+
+    options.save().then(function(){
+        $('.notification').show();
+        setTimeout(function(){
+            $('.notification').hide();
+        }, 3000)
 
     });
 
 
-    $('#saveButton').click(function(){
-        var options = new OptionsRepo.getInstance();
-
-        options.axoSoftUrl = $('#axoSoftUrl').val();
-        options.axoSoftToken = $('#axoSoftToken').val();
-        options.axoSoftUserId = $('#axoSoftUserId').val();
-
-        options.save();
-
-
-    })
+})
 
 
 
